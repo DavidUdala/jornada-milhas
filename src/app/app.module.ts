dfeee6d6088768296e05466d30c3d994b754bd43
DavidUdala <david_udala@hotmail.com>
2024-06-25 15:48:35 -0300

    Finalizado desafio aula 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -69,7 +69,12 @@ registerLocaleData(localePt, 'pt');
     MatDialogModule,
     HttpClientModule
   ],
-  providers: [{provide: LOCALE_ID, useValue: 'pt'}],
+  providers: [{provide: LOCALE_ID, useValue: 'pt'},
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { PromocaoService } from 'src/app/core/services/promocao.service';
+import { Promocao } from 'src/app/core/types/types';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
+
+  @Output() promocoes: Promocao[] = [];
 
   constructor(
     private servicePromocao: PromocaoService
   ) { }
 
+
   ngOnInit(): void {
     this.servicePromocao.listar().subscribe({
       next: result => {
         console.log(result);
+        this.promocoes = result;
       },
       error: err => {
         console.error(err);
